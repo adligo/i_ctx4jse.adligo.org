b25380f390d76d279c7469643045bf852b468fd6
Scott Morgan <scott@adligo.com>
2022-01-12 18:47:08 -0600

    Formatted.

--- src/org/adligo/i/ctx4jse/shared/I_PrintCtx.java
+++ src/org/adligo/i/ctx4jse/shared/I_PrintCtx.java
@@ -5,18 +5,22 @@ import java.io.PrintStream;
 import org.adligo.i.ctx.shared.I_Ctx;
 
 /**
- * This extends the I_JseCtx with print methods that wrap the
- * System's {@link PrintStream#println(Object)} and 
- *  {@link PrintStream#print(Object)}  methods.
- * Like all of Adligp's Context (abbreviated Ctx) facilitate the Context Creation and
- * Contextualitan design patterns.  These patterns allow stubbing with tools like;
- * {@link <a href="https://github.com/adligo/mockito_ext.adligo.org">mockito_ext.adligo.org</a>}<br/><br/>
- *   Note this is intentionally broken away from the I_JseCtx so that some systems MAY 
- *  intentionally discourage the use of these print and println methods.<br/><br/>
+ * This extends the I_JseCtx with print methods that wrap the System's
+ * {@link PrintStream#println(Object)} and {@link PrintStream#print(Object)}
+ * methods. Like all of Adligp's Context (abbreviated Ctx) facilitate the
+ * Context Creation and Contextualitan design patterns. These patterns allow
+ * stubbing with tools like; {@link <a href=
+ * "https://github.com/adligo/mockito_ext.adligo.org">mockito_ext.adligo.org</a>}<br/>
+ * <br/>
+ * Note this is intentionally broken away from the I_JseCtx so that some systems
+ * MAY intentionally discourage the use of these print and println methods.<br/>
+ * <br/>
  * 
  * @see {@link I_Ctx}
  * @author scott<br/>
- * <pre><code>
+ * 
+ *         <pre>
+ * <code>
  *  ---------------- Apache ICENSE-2.0 --------------------------
  *
  * Copyright 2022 Adligo Inc
@@ -32,15 +36,17 @@ import org.adligo.i.ctx.shared.I_Ctx;
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * </code></pre>
+ * </code>
+ *         </pre>
  */
 
 public interface I_PrintCtx extends I_JseCtx {
 
   /**
-   * This wraps the {@link} System}'s {@link PrintStream#print(Object)}
-   * so that it can be easily stubbed with tools like;
-   * {@link <a href="https://github.com/adligo/mockito_ext.adligo.org">mockito_ext.adligo.org</a>}
+   * This wraps the {@link} System}'s {@link PrintStream#print(Object)} so that it
+   * can be easily stubbed with tools like; {@link <a href=
+   * "https://github.com/adligo/mockito_ext.adligo.org">mockito_ext.adligo.org</a>}
+   * 
    * @param message
    * @return
    */
@@ -48,11 +54,12 @@ public interface I_PrintCtx extends I_JseCtx {
     System.out.println(message);
     return this;
   }
-  
+
   /**
-   * This wraps the {@link} System}'s {@link PrintStream#print(String)}
-   * so that it can be easily stubbed with tools like;
-   * {@link <a href="https://github.com/adligo/mockito_ext.adligo.org">mockito_ext.adligo.org</a>}
+   * This wraps the {@link} System}'s {@link PrintStream#print(String)} so that it
+   * can be easily stubbed with tools like; {@link <a href=
+   * "https://github.com/adligo/mockito_ext.adligo.org">mockito_ext.adligo.org</a>}
+   * 
    * @param message
    * @return
    */
@@ -60,11 +67,12 @@ public interface I_PrintCtx extends I_JseCtx {
     System.out.println(message);
     return this;
   }
-  
+
   /**
-   * This wraps the {@link} System}'s {@link PrintStream#println(Object)}
-   * so that it can be easily stubbed with tools like;
-   * {@link <a href="https://github.com/adligo/mockito_ext.adligo.org">mockito_ext.adligo.org</a>}
+   * This wraps the {@link} System}'s {@link PrintStream#println(Object)} so that
+   * it can be easily stubbed with tools like; {@link <a href=
+   * "https://github.com/adligo/mockito_ext.adligo.org">mockito_ext.adligo.org</a>}
+   * 
    * @param message
    * @return
    */
@@ -74,9 +82,10 @@ public interface I_PrintCtx extends I_JseCtx {
   }
 
   /**
-   * This wraps the {@link} System}'s {@link PrintStream#println(String)}
-   * so that it can be easily stubbed with tools like;
-   * {@link <a href="https://github.com/adligo/mockito_ext.adligo.org">mockito_ext.adligo.org</a>}
+   * This wraps the {@link} System}'s {@link PrintStream#println(String)} so that
+   * it can be easily stubbed with tools like; {@link <a href=
+   * "https://github.com/adligo/mockito_ext.adligo.org">mockito_ext.adligo.org</a>}
+   * 
    * @param message
    * @return
    */
